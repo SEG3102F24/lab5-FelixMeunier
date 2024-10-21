94cbe39088f08c98147f5eb3129abaa4b1167e06
FelixMeunier <fmeun013@uottawa.ca>
2024-10-20 21:43:53 -0400

    Added firebase configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,19 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
+import {getFirestore, provideFirestore} from "@angular/fire/firestore";
+import {environment} from "../environments/environment.development";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
+
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
+    ]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+      apiKey: "AIzaSyA2-WhIkzySE00-_H1leQxuY5LdhxMtMYM",
+      authDomain: "lab5-felixmeunier.firebaseapp.com",
+      projectId: "lab5-felixmeunier",
+      storageBucket: "lab5-felixmeunier.appspot.com",
+      messagingSenderId: "60197602669",
+      appId: "1:60197602669:web:984969d5909f6d00618664",
+      measurementId: "G-5KPY4SPE5B"
+    }
+
+  };
